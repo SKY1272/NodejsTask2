2356cd58d5259a74b42a7213243d66dffae1f1b3
SKY1272 <MY_NAME@example.com>
2025-05-25 00:48:08 +0530

    Initial commit

--- Server.js
+++ Server.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const app=express();
-const port=3000;
-app.get('/',(req,res)=>{
-  res.send('Server is up and running on port 3000! Ready to handle requests.');
+const port=4000;
+function addUserMiddleware(req,res,next){
+  req.user='Guest';
+  next();
+}
+app.get('/welcome',addUserMiddleware,(req,res)=>{
+  res.send(`<h1>Welcome ${req.user}</h1>`)
 })
 app.listen(port,()=>{
   console.log(`Example app listening at http://localhost:${port}`);
