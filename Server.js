d640a04409da0d0b325871f70594a0166962106f
SKY1272 <MY_NAME@example.com>
2025-05-25 16:16:52 +0530

    first commit

--- Server.js
+++ Server.js
@@ -1,13 +1,22 @@
 const express=require('express');
 const app=express();
 const port=4000;
-function addUserMiddleware(req,res,next){
-  req.user='Guest';
-  next();
-}
-app.get('/welcome',addUserMiddleware,(req,res)=>{
-  res.send(`<h1>Welcome ${req.user}</h1>`)
+ app.use(express.json());
+ app.get('/', (req, res) => {
+  res.send('Welcome to the Express Server!');
+});
+ app.get('/orders',(req,res)=>{
+     res.send('Here is the list of all orders.')
+ })
+app.post('/orders',(req,res)=>{
+  res.send('A new order has been created.')
+})
+app.get('/users',(req,res)=>{
+  res.send('Here is the list of all users.')
+})
+app.post('/users',(req,res)=>{
+  res.send('A new user has been created.')
 })
 app.listen(port,()=>{
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`server is running at http://localhost:${port}`)
+})
